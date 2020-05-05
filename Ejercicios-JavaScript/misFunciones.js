861a7240efcfe94dd44a0a42e65556504a478501
MartinaCeballos <1923373@ucc.edu.ar>
2020-05-05 17:50:55 -0300

    Se agrego ejercicio de grados y radianes

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -10,27 +10,40 @@
  * @return
  */
 function CambioUnidades(id, value) {
-    if(isNaN(value)){
+    if (isNaN(value)) {
         alert("Se ingreso un valor invalido");
-        document.Unidades.unid_metro.value= "";
-        document.Unidades.unid_pulgada.value= "";
-        document.Unidades.unid_pie.value= "";
-        document.Unidades.unid_yarda.value="";
-    }else if(id=="metro"){
-        document.Unidades.unid_pulgada.value= 39.3701*value;
-        document.Unidades.unid_pie.value= 3.28084*value;
-        document.Unidades.unid_yarda.value=1.09361*value;
-    }else if(id=="pulgada") {
-        document.Unidades.unid_metro.value = 0.0254*value;
-        document.Unidades.unid_pie.value = 0.0833333*value;
-        document.Unidades.unid_yarda.value = 0.0277778*value;
-    }else if(id=="pie"){
-        document.Unidades.unid_metro.value= 0.3084*value;
-        document.Unidades.unid_pulgada.value= 12*value;
-        document.Unidades.unid_yarda.value=0.3333333*value;
-    }else if(id=="yarda"){
-        document.Unidades.unid_metro.value= 0.9144*value;
-        document.Unidades.unid_pulgada.value= 36*value;
-        document.Unidades.unid_pie.value=3*value;
+        document.Unidades.unid_metro.value = "";
+        document.Unidades.unid_pulgada.value = "";
+        document.Unidades.unid_pie.value = "";
+        document.Unidades.unid_yarda.value = "";
+    } else if (id == "metro") {
+        document.Unidades.unid_pulgada.value = 39.3701 * value;
+        document.Unidades.unid_pie.value = 3.28084 * value;
+        document.Unidades.unid_yarda.value = 1.09361 * value;
+    } else if (id == "pulgada") {
+        document.Unidades.unid_metro.value = 0.0254 * value;
+        document.Unidades.unid_pie.value = 0.0833333 * value;
+        document.Unidades.unid_yarda.value = 0.0277778 * value;
+    } else if (id == "pie") {
+        document.Unidades.unid_metro.value = 0.3084 * value;
+        document.Unidades.unid_pulgada.value = 12 * value;
+        document.Unidades.unid_yarda.value = 0.3333333 * value;
+    } else if (id == "yarda") {
+        document.Unidades.unid_metro.value = 0.9144 * value;
+        document.Unidades.unid_pulgada.value = 36 * value;
+        document.Unidades.unid_pie.value = 3 * value;
     }
+}
+
+function ConvertirGaR(id) {
+    var gra, rad;
+    if(id=="grados"){
+        grad = document.getElementById ( elementid="grados" ).value;
+        rad = (grad*Math.PI)/180;
+    }else if(id=="radianes"){
+        rad=document.getElementById (elementid="radianes").value;
+        grad=(rad*180)/Math.PI;
+    }
+    document.getElementById (elementid="grados").value=grad;
+    document.getElementById (elementid="radianes").value=rad;
 }
