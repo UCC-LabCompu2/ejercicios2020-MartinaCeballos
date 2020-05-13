7572c296ea5cc56a1e341a0bc8c836951cad6457
MartinaCeballos <1923373@ucc.edu.ar>
2020-05-13 16:33:14 -0300

    Se agrego ejercicio de matematicas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -37,13 +37,46 @@ function CambioUnidades(id, value) {
 
 function ConvertirGaR(id) {
     var gra, rad;
-    if(id=="grados"){
-        grad = document.getElementById ( elementid="grados" ).value;
-        rad = (grad*Math.PI)/180;
-    }else if(id=="radianes"){
-        rad=document.getElementById (elementid="radianes").value;
-        grad=(rad*180)/Math.PI;
+    if (id == "grados") {
+        grad = document.getElementById(elementid = "grados").value;
+        rad = (grad * Math.PI) / 180;
+    } else if (id == "radianes") {
+        rad = document.getElementById(elementid = "radianes").value;
+        grad = (rad * 180) / Math.PI;
     }
-    document.getElementById (elementid="grados").value=grad;
-    document.getElementById (elementid="radianes").value=rad;
+    document.getElementById(elementid = "grados").value = grad;
+    document.getElementById(elementid = "radianes").value = rad;
+}
+
+function mostrar_ocultar(valorMO) {
+    if (valorMO == "val_mostrar") {
+        document.getElementById(elementid = "divMO").style.display = 'block';
+    } else if (valorMO == "val_ocultar") {
+        document.getElementById(elementid = "divMO").style.display = 'none';
+    }
+}
+
+function calcular_suma() {
+    var nums1, nums2;
+    nums1= Number(document.getElementsByName(elementName="sum_num1")[0].value);
+    nums2= document.getElementsByName(elementName="sum_num2")[0].value;
+    document.getElementsByName(elementName="sum_total")[0].value=nums1+Number(nums2);
+}
+function calcular_resta() {
+    var numr1, numr2;
+    numr1= Number(document.getElementsByName(elementName="res_num1")[0].value);
+    numr2= document.getElementsByName(elementName="res_num2")[0].value;
+    document.getElementsByName(elementName="res_total")[0].value=numr1-Number(numr2);
+}
+function calcular_multiplicacion() {
+    var numm1, numm2;
+    numm1= Number(document.getElementsByName(elementName="mul_num1")[0].value);
+    numm2= document.getElementsByName(elementName="mul_num2")[0].value;
+    document.getElementsByName(elementName="mul_total")[0].value=numm1*Number(numm2);
+}
+function calcular_division() {
+    var numd1, numd2;
+    numd1= Number(document.getElementsByName(elementName="div_num1")[0].value);
+    numd2= document.getElementsByName(elementName="div_num2")[0].value;
+    document.getElementsByName(elementName="div_total")[0].value=numd1/Number(numd2);
 }
